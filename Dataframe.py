ec44b0dc07ed34c74d02fa8e2d5db5b876e8be20
Vedang <vedangraut1123@gmail.com>
2025-10-29 05:12:52 +0530

    This is practice commit

--- Dataframe.py
+++ Dataframe.py
@@ -21,5 +21,41 @@ majorsales=pd.DataFrame({'c1':sales1,
 #print(majorsales.shape)
 
 #let us access the the data with coumn name
-print(sales.loc[1,'Profession'])  
-print(sales.iloc[0,1]) 
+#print(sales.loc[1,'Profession'])  
+#print(sales.iloc[0,1]) 
+
+
+
+
+Powerlifters=pd.DataFrame({
+    "Names":["Rupam","Manav","Swaraj","Hitanshu"],
+    "Lifts":[260,200,200,250]
+})
+
+
+
+#ssf=Powerlifters[Powerlifters['Lifts']>250]
+#print(ssf)
+
+
+#
+
+#print(Powerlifters)
+
+#Powerlifters.loc[Powerlifters['Lifts']==260,250]=270
+
+
+Powerlifters.rename(columns={"Names":"Giants"},inplace=True)
+
+
+
+#djfc=Powerlifters.loc[Powerlifters["Giants"]=="Rupam","Giants"]="simpleshotz"
+
+#ssf=Powerlifters.loc[Powerlifters["Lifts"]==260,"Lifts"]=280
+#ssf=Powerlifters.rename(columns={"Giants":"ANGER"},inplace=True)
+
+
+Powerlifters["GYM"]=["SSF","DJFC","EXplore","Tauras"]
+#print(Powerlifters)
+
+print(Powerlifters[Powerlifters["Lifts"]>200])
---
+++ Practicecurrent.py
@@ -0,0 +1,26 @@
+import pandas as pd
+GYM=pd.DataFrame({
+    "Names":["Rupam","Manav","Swaraj","Hitanshu"],
+    "Lifts":[260,200,210,260]
+})
+
+ssf=GYM[GYM["Lifts"]>200]
+#print(ssf)
+
+
+GYM["Location"]=["Palghar","Palghar","Palghar","Palghar"]
+
+#ssf=Powerlifters.rename(columns={"Giants":"ANGER"},inplace=True)
+GYM.rename(columns={"Location":"place"},inplace=True)
+
+
+GYM.loc[GYM['place']=="Palghar","place"]="umroli"
+
+
+
+
+
+GYM.rename(columns={"place":"Locationarea"},inplace=True)
+
+GYM.loc[GYM["Names"]=="Rupam","Names"]="simpleshotz"
+print(GYM) 
