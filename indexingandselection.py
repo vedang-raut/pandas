96c6c78a784f30770fee606b197e9273ced80082
Vedang <vedangraut1123@gmail.com>
2025-10-30 00:55:35 +0530

    This consist of the multiindex and condition and query

--- indexingandselection.py
+++ indexingandselection.py
@@ -8,8 +8,19 @@ sales['Places']=["Boisar","Umroli","Palghar"]
 #In this line we have set the index to the Places
 sales.set_index("Places",inplace=True)
 #Here we have fetched the value using the index value
-#print(sales.loc["Boisar","Names"])
-
+'''print(sales.loc["Boisar","Names"])'''
+#Through this we have reset the index to default
 sales.reset_index(inplace=True)
 
-print(sales)
+#here we have added the multiple indexes within the dataframe.
+companies=pd.DataFrame({
+    "Names":["IBM","EY","Goldmansachs","Infosys","Capgemini"],
+    "Location":["USA","Germany","Europe","India","Uk"],
+    "Profession":["Tech","Fin","Fin","Tech","Tech"],
+    "Salary":[22,20,10,23,13]
+}).set_index(["Location","Profession"])
+
+#This is using the condition 
+(companies[companies["Salary"]>=12])
+
+print(companies.query('Salary>12'))
